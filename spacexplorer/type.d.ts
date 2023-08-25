2db44ba109250d7c74ae619db524f2936da493de
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-08-25 16:29:29 +0000

    Update Capsule props

--- spacexplorer/type.d.ts
+++ spacexplorer/type.d.ts
@@ -1,15 +1,21 @@
 export {};
 
 declare global {
+  interface Mission {
+    name: string;
+    flight: number;
+  }
+
   interface Capsule {
-    reuse_count: number;
-    water_landings: number;
-    land_landings: number;
-    last_update: string;
-    launches: string[];
-    serial: string;
-    status: string;
+    capsule_serial: string;
+    capsule_id: string;
+    status: 'active' | 'retired' | 'unknown' | 'destroyed';
+    original_launch: string;
+    original_launch_unix: number;
+    missions: Mission[];
+    landings: number;
     type: string;
-    id: string;
+    details: string;
+    reuse_count: number;
   }
 }
