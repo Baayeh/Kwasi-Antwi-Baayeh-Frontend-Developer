6bee1c4421b37208acf9268e234f830a53150381
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-08-25 10:46:18 +0000

    Add custom theme to tailwindcss

--- spacexplorer/tailwind.config.js
+++ spacexplorer/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'banner-img':
+          "url('https://res.cloudinary.com/dskl0qde4/image/upload/v1678474160/space_s8mxgr.jpg')",
+        'main-img':
+          "url('https://res.cloudinary.com/dskl0qde4/image/upload/v1678482474/philip-myrtorp-HPNLQH3UNSM-unsplash_tp61kn.jpg')",
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-20deg)' },
+          '50%': { transform: 'rotate(20deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 3s ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
 };
