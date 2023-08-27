58bb410d893b85d21949dac0a5c5aaad38378f09
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-08-27 22:30:58 +0000

    Update the global types file

--- spacexplorer/type.d.ts
+++ spacexplorer/type.d.ts
@@ -9,13 +9,16 @@ declare global {
   interface Capsule {
     capsule_serial: string;
     capsule_id: string;
-    status: 'active' | 'retired' | 'unknown' | 'destroyed';
+    status: string;
     original_launch: string;
     original_launch_unix: number;
     missions: Mission[];
     landings: number;
     type: string;
-    details: string;
+    details: string | null;
     reuse_count: number;
   }
+
+  type Status = 'active' | 'retired' | 'unknown' | 'destroyed';
+  type CapsuleType = 'Dragon 1.0' | 'Dragon 1.1' | 'Dragon 2.0';
 }
