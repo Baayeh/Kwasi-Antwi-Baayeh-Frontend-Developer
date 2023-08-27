3bce9931727f3cb07aef3672d757b10b3f0f7f17
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-08-27 22:31:27 +0000

    Create sample data for test cases

---
+++ spacexplorer/src/sampleData.ts
@@ -0,0 +1,87 @@
+export const data: Capsule[] = [
+  {
+    capsule_serial: 'C101',
+    capsule_id: 'dragon1',
+    status: 'retired',
+    original_launch: '2010-12-08T15:43:00.000Z',
+    original_launch_unix: 1291822980,
+    missions: [
+      {
+        name: 'COTS 1',
+        flight: 7,
+      },
+    ],
+    landings: 0,
+    type: 'Dragon 1.0',
+    details: 'Reentered after three weeks in orbit',
+    reuse_count: 0,
+  },
+  {
+    capsule_serial: 'C102',
+    capsule_id: 'dragon1',
+    status: 'retired',
+    original_launch: '2012-05-02T07:44:00.000Z',
+    original_launch_unix: 1335944640,
+    missions: [
+      {
+        name: 'COTS 2',
+        flight: 8,
+      },
+    ],
+    landings: 1,
+    type: 'Dragon 1.0',
+    details: 'First Dragon spacecraft',
+    reuse_count: 0,
+  },
+  {
+    capsule_serial: 'C103',
+    capsule_id: 'dragon1',
+    status: 'unknown',
+    original_launch: '2012-10-08T00:35:00.000Z',
+    original_launch_unix: 1349656500,
+    missions: [
+      {
+        name: 'CRS-1',
+        flight: 9,
+      },
+    ],
+    landings: 1,
+    type: 'Dragon 1.0',
+    details: 'First of twenty missions flown under the CRS1 contract',
+    reuse_count: 0,
+  },
+  {
+    capsule_serial: 'C104',
+    capsule_id: 'dragon1',
+    status: 'unknown',
+    original_launch: '2013-03-01T19:10:00.000Z',
+    original_launch_unix: 1362165000,
+    missions: [
+      {
+        name: 'CRS-2',
+        flight: 10,
+      },
+    ],
+    landings: 1,
+    type: 'Dragon 1.0',
+    details: null,
+    reuse_count: 0,
+  },
+  {
+    capsule_serial: 'C105',
+    capsule_id: 'dragon1',
+    status: 'unknown',
+    original_launch: '2014-04-18T19:25:00.000Z',
+    original_launch_unix: 1397849100,
+    missions: [
+      {
+        name: 'CRS-3',
+        flight: 14,
+      },
+    ],
+    landings: 1,
+    type: 'Dragon 1.1',
+    details: 'First Dragon v1.1 capsule',
+    reuse_count: 0,
+  },
+];
