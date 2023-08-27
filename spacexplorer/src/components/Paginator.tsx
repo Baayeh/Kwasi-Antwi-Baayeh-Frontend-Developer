f2bee1c35f671013946c6e29779478468149c0b2
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-08-27 22:29:48 +0000

    Update Paginator component to update the page number

--- spacexplorer/src/components/Paginator.tsx
+++ spacexplorer/src/components/Paginator.tsx
@@ -13,29 +13,31 @@ const Paginator: React.FC<PaginatorProps> = ({
   setCurrentPage,
 }) => {
   return (
-    <div className="flex items-center justify-end gap-x-3 mb-3 me-2">
-      <button
-        title="Previous"
-        type="button"
-        onClick={() => setCurrentPage(currentPage - 1)}
-        disabled={currentPage === 1}
-        className="disabled:cursor-not-allowed disabled:text-gray-500"
-      >
-        <span>
-          <BsArrowLeftSquareFill size={24} />
-        </span>
-      </button>
-      <button
-        title="Next"
-        type="button"
-        onClick={() => setCurrentPage(currentPage + 1)}
-        disabled={currentPage === totalPages}
-        className="disabled:cursor-not-allowed disabled:text-gray-500"
-      >
-        <span>
-          <BsArrowRightSquareFill size={24} />
-        </span>
-      </button>
+    <div className="lg:w-[60rem] mx-auto">
+      <div className="flex items-center justify-end gap-x-3 mb-3 me-2">
+        <button
+          title="Previous"
+          type="button"
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="disabled:cursor-not-allowed disabled:text-gray-500"
+        >
+          <span>
+            <BsArrowLeftSquareFill size={24} />
+          </span>
+        </button>
+        <button
+          title="Next"
+          type="button"
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className="disabled:cursor-not-allowed disabled:text-gray-500"
+        >
+          <span>
+            <BsArrowRightSquareFill size={24} />
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
